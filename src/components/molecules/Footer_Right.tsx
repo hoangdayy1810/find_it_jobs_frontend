7ccf56ae798c6a47fbcc080b36fd176be2d11f70
fpt <hoangvietnguyen1810@gmail.com>
2025-06-09 16:12:03 +0700

    Feat: Update footer components with internationalization support and dynamic content

--- src/components/molecules/Footer_Right.tsx
+++ src/components/molecules/Footer_Right.tsx
@@ -1,48 +1,59 @@
 import React from "react";
 import Footer_List_Li from "../atoms/Footer_List_Li";
-// import Logo from "../atoms/Logo";
 
-const Footer_Right = () => {
+const Footer_Right = ({ t }: { t: any }) => {
   return (
     <div className="mt-2 md:mt-0 md:pl-10">
       <div className="grid gap-4 lg:grid-cols-3 grid-cols-2 md:gap-16">
         <div>
           <div className="mb-2">
-            <h3 className="font-bold text-lg">DỊCH VỤ</h3>
+            <h3 className="font-bold text-lg">
+              {t("footer.footer-right.service.name").toUpperCase()}
+            </h3>
           </div>
           <div>
             <ul>
-              <Footer_List_Li text="Điều khoản sử dụng" />
-              <Footer_List_Li text="Chính sách bảo mật thông tin cá nhân" />
-              <Footer_List_Li text="Chính sách bảo mật thanh toán" />
-              <Footer_List_Li text="Giới thiệu Fahasa" />
-              <Footer_List_Li text="Hệ thống trung tâm - nhà sách" />
+              <Footer_List_Li text={t("footer.footer-right.service.item-1")} />
+              <Footer_List_Li text={t("footer.footer-right.service.item-2")} />
+              <Footer_List_Li text={t("footer.footer-right.service.item-3")} />
+              <Footer_List_Li text={t("footer.footer-right.service.item-4")} />
             </ul>
           </div>
         </div>
         <div>
           <div className="mb-2">
-            <h3 className="font-bold text-lg">HỖ TRỢ</h3>
+            <h3 className="font-bold text-lg">
+              {t("footer.footer-right.support.name").toUpperCase()}
+            </h3>
           </div>
           <div>
             <ul>
-              <Footer_List_Li text="Chính sách đổi - trả - hoàn tiền" />
-              <Footer_List_Li text="Chính sách bảo hành - bồi hoàn" />
-              <Footer_List_Li text="Chính sách vận chuyển" />
-              <Footer_List_Li text="Chính sách khách sỉ" />
+              <Footer_List_Li text={t("footer.footer-right.support.item-1")} />
+              <Footer_List_Li text={t("footer.footer-right.support.item-2")} />
+              <Footer_List_Li text={t("footer.footer-right.support.item-3")} />
             </ul>
           </div>
         </div>
         <div>
           <div className="mb-2">
-            <h3 className="font-bold text-lg">TÀI KHOẢN CỦA TÔI</h3>
+            <h3 className="font-bold text-lg">
+              {t("footer.footer-right.my-account.name").toUpperCase()}
+            </h3>
           </div>
           <div>
             <ul>
-              <Footer_List_Li text="Đăng nhập/Tạo mới tài khoản" />
-              <Footer_List_Li text="Thay đổi địa chỉ khách hàng" />
-              <Footer_List_Li text="Chi tiết tài khoản" />
-              <Footer_List_Li text="Lịch sử mua hàng" />
+              <Footer_List_Li
+                text={t("footer.footer-right.my-account.item-1")}
+              />
+              <Footer_List_Li
+                text={t("footer.footer-right.my-account.item-2")}
+              />
+              <Footer_List_Li
+                text={t("footer.footer-right.my-account.item-3")}
+              />
+              <Footer_List_Li
+                text={t("footer.footer-right.my-account.item-4")}
+              />
             </ul>
           </div>
         </div>
@@ -50,38 +61,27 @@ const Footer_Right = () => {
 
       <div className="mt-4">
         <div className="mb-2">
-          <h3 className="font-bold text-lg">LIÊN HỆ</h3>
+          <h3 className="font-bold text-lg">
+            {t("footer.footer-right.contact.name").toUpperCase()}
+          </h3>
         </div>
         <div>
           <ul className="grid grid-cols-1 md:grid-cols-3 md:gap-16">
             <li className="text-sm py-2">
               <i className="fa-solid fa-location-dot fa-lg mr-1"></i>
-              60-62 Lê Lợi, Q.1, TP. HCM
+              {t("footer.footer-right.contact.item-1")}
             </li>
             <li className="text-sm py-2">
               <i className="fa-solid fa-envelope fa-lg mr-1"></i>
-              cskh@fahasa.com.vn
+              {t("footer.footer-right.contact.item-3")}
             </li>
             <li className="text-sm py-2">
               <i className="fa-solid fa-phone fa-lg mr-1"></i>
-              1900636467
+              {t("footer.footer-right.contact.item-2")}
             </li>
           </ul>
         </div>
       </div>
-
-      {/* <div className='flex justify-around items-center py-2'>
-                <Logo href='https://vnpay.vn/'
-                    src='/images/footer/Logo_NCC/vnpay_logo.png'
-                    style='w-32'
-                    target='_blank'
-                />
-                <Logo href='https://www.momo.vn/'
-                    src='/images/footer/Logo_NCC/momopay.png'
-                    style='w-16'
-                    target='_blank'
-                />
-            </div> */}
     </div>
   );
 };
--- src/components/templates/Footer.tsx
+++ src/components/templates/Footer.tsx
@@ -1,16 +1,20 @@
+"use client";
+
 import React from "react";
 import Footer_Left from "../molecules/Footer_Left";
 import Footer_Right from "../molecules/Footer_Right";
+import { useTranslations } from "next-intl";
 
 const Footer = () => {
+  const t = useTranslations();
   return (
     <div className="w-full p-4 text-black md:bg-[#FFFFFF]">
       <div className="mx-auto md:flex md:justify-center md:items-center md:w-5/6">
         <div className="md:w-1/3">
-          <Footer_Left />
+          <Footer_Left t={t} />
         </div>
         <div className="md:w-2/3">
-          <Footer_Right />
+          <Footer_Right t={t} />
         </div>
       </div>
     </div>
