78c24a6a009e1e2e36ac0afa4896d906dd5b955a
fpt <hoangvietnguyen1810@gmail.com>
2025-06-15 18:59:06 +0700

    Fix: update call api

--- src/utils/axiosInstance.ts
+++ src/utils/axiosInstance.ts
@@ -1,21 +1,39 @@
-import axios from 'axios';
-
+import axios from "axios";
+import nookies from "nookies";
 
 const axiosInstance = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_URL_BACKEND,
-    withCredentials: true,
+  baseURL: process.env.NEXT_PUBLIC_URL_BACKEND,
+  withCredentials: true,
+});
+
+// Lấy token từ cookie và thêm vào Authorization header
+axiosInstance.interceptors.request.use((config) => {
+  const cookies = nookies.get(); // get tất cả cookies
+  const token = cookies.token;
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  console.log("Request config:", config);
+
+  return config;
 });
 
 axiosInstance.interceptors.response.use(
-    (response) => {
-        return response;
-    },
-    (error) => {
-        if (error.response && (error.response.status === 500) && typeof error.response.data === 'object') {
-            alert(error.response.data.message || "Có lỗi xảy ra (404 | 500)");
-        }
-        return Promise.reject(error);
+  (response) => {
+    return response;
+  },
+  (error) => {
+    if (
+      error.response &&
+      error.response.status === 500 &&
+      typeof error.response.data === "object"
+    ) {
+      alert(error.response.data.message || "Có lỗi xảy ra (404 | 500)");
     }
+    return Promise.reject(error);
+  }
 );
 
 export default axiosInstance;
